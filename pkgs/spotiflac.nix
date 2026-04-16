e87ed3f98b9ce484fe7ab47083eaad19bec6f978
shadowv60 <shadowvpsl48@gmail.com>
2026-04-16 17:53:33 +0530

    spotiflc tar

--- pkgs/spotiflac.nix
+++ pkgs/spotiflac.nix
@@ -1,38 +1,50 @@
 {
   stdenv,
-  gst_all_1,
   fetchurl,
   appimage-run,
   webkitgtk_4_1,
   makeWrapper,
   ffmpeg,
+  gst_all_1,
   glib-networking,
+  lib,
 }:
-
 stdenv.mkDerivation rec {
   pname = "spotiflac";
-  version = "7.1.3";
+  version = "7.1.4";
 
   src = fetchurl {
-    url = "https://github.com/afkarxyz/SpotiFLAC/releases/download/v${version}/SpotiFLAC.AppImage";
-    hash = "sha256-3IjUblz61MuternDg2oqfkrs/gSlq0I9oqmE0oZGrsM=";
+    url = "https://github.com/spotbye/SpotiFLAC/releases/download/v${version}/spotiflac-linux-bundle.tar.gz";
+    hash = "sha256-5L+okTBF4nJ+60O6y8YrPeSrpcNN3gxjFPVKdEyR8QU=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
-  dontUnpack = true;
-  dontBuild = true;
+  # This avoids the manual 'tar' in unpackPhase
+  sourceRoot = ".";
 
   installPhase = ''
+    runHook preInstall
+
+    # Create a destination for the AppImage
+    mkdir -p $out/opt/spotiflac
+    cp SpotiFLAC-linux-bundle/SpotiFLAC-amd64.AppImage $out/opt/spotiflac/spotiflac.AppImage
+    chmod +x $out/opt/spotiflac/spotiflac.AppImage
+
+    # Use makeWrapper to call appimage-run on the specific path
     mkdir -p $out/bin
-    ln -s $src $out/bin/SpotiFLAC.AppImage
     makeWrapper ${appimage-run}/bin/appimage-run $out/bin/spotiflac \
-      --add-flags "$out/bin/SpotiFLAC.AppImage" \
-      --prefix LD_LIBRARY_PATH : "${webkitgtk_4_1}/lib" \
-      --prefix PATH : "${ffmpeg}/bin" \
-      --prefix GST_PLUGIN_PATH : "${gst_all_1.gst-plugins-base}/lib/gstreamer-1.0" \
+      --add-flags "$out/opt/spotiflac/spotiflac.AppImage" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ webkitgtk_4_1 ]}" \
+      --prefix PATH : "${lib.makeBinPath [ ffmpeg ]}" \
+      --prefix GST_PLUGIN_PATH : "${gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${gst_all_1.gst-plugins-good}/lib/gstreamer-1.0" \
       --prefix GIO_EXTRA_MODULES : "${glib-networking}/lib/gio/modules"
+
+    runHook postInstall
   '';
 
-  meta.platforms = [ "x86_64-linux" ];
+  meta = {
+    description = "Spotify to FLAC downloader";
+    platforms = [ "x86_64-linux" ];
+  };
 }
